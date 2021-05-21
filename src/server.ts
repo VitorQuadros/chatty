2cf14df32b2d46f3fb8f195b338a3790e1b74e6a
vitor <viitorquadros@hotmail.com>
2021-05-21 11:15:34 -0300

    create (server): ws connection

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
 import express, { json } from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+
 import './database';
 import { routes } from './routes';
 
 const app = express();
 
+const http = createServer(app); // create http protocol
+const io = new Server(http); // create ws protocol
+
+io.on('connection', (socket: Socket) => {
+  console.log('connected', socket.id);
+});
+
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+http.listen(3333, () => {
   console.log('Running...');
 });
