cc86d6d3fea03c1a13174f92c9fa0cac4b3ed22e
vitor <viitorquadros@hotmail.com>
2021-05-21 11:38:16 -0300

    feat (socket): emit first_access

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,5 @@
-import express, { json } from 'express';
-import { createServer } from 'http';
-import { Server, Socket } from 'socket.io';
-
-import './database';
-import { routes } from './routes';
-import path from 'path';
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, '..', 'public')));
-app.set('views', path.join(__dirname, '..', 'public'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
-app.get('/pages/client', (req, res) => {
-  return res.render('html/client.html');
-});
-
-const http = createServer(app); // create http protocol
-const io = new Server(http); // create ws protocol
-
-io.on('connection', (socket: Socket) => {
-  console.log('connected', socket.id);
-});
-
-app.use(express.json());
-app.use(routes);
+import { http } from './http';
+import './websocket/client';
 
 http.listen(3333, () => {
   console.log('Running...');
