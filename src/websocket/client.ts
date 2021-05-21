d38136776c58f4413aaf62fcdf30e9bd38c52585
vitor <viitorquadros@hotmail.com>
2021-05-21 11:48:08 -0300

    create: connection entity

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -2,6 +2,6 @@ import { io } from '../http';
 
 io.on('connect', (socket) => {
   socket.on('client_first_access', (params) => {
-    console.log(params);
+    // Save connection with socket_id, user_id
   });
 });
