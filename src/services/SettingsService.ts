42e0beaa7d2c78829c2dd26d68d110bd22c9bcea
vitor <viitorquadros@hotmail.com>
2021-05-21 12:20:02 -0300

    feat (settings service): find by username method

--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -31,6 +31,14 @@ class SettingsService {
     await this.settingsRepository.save(settings);
     return settings;
   }
+
+  async findByUsername(username: string) {
+    const settings = await this.settingsRepository.findOne({
+      username,
+    });
+
+    return settings;
+  }
 }
 
 export { SettingsService };
